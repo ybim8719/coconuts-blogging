019cef62f57d8fdcbe8e985d4f47d65fbf9d880a
PascalThao <thaopascal@gmail.com>
2020-03-30 16:06:02 +0200

    small fix

--- src/Controller/GeneralPagesController.php
+++ src/Controller/GeneralPagesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Entity\User;
+use App\Service\Helper\DateCalculator;
 use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,11 +19,15 @@ class GeneralPagesController extends AbstractController
 {
     private $em;
     private $userRepository;
+    private $articleRepository;
+    private $dateHelper;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, DateCalculator $calculator)
     {
         $this->userRepository = $em->getRepository(User::class);
+        $this->articleRepository = $em->getRepository(Article::class);
         $this->em = $em;
+        $this->dateHelper = $calculator;
     }
 
     /**
@@ -30,6 +35,9 @@ class GeneralPagesController extends AbstractController
      */
     public function publicHomePage()
     {
+        $article = $this->articleRepository->find(7);
+        dump($article);
+        dump($this->dateHelper->convertToAdaptedDuration($article->getCreatedAt()));
         return $this->render('general/public_home_page.html.twig', [
         ]);
     }
